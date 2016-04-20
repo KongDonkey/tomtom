1d9e48a145134529cc54565af091aa57089e5d63
KongDonkey <edonkey2@rambler.ru>
2016-04-20 15:23:34 +0300

    tom easy servlet exampl

---
+++ JA.TOMCAT/src/main/java/tom/servlet/Second.java
@@ -0,0 +1,42 @@
+package tom.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Second
+ */
+public class Second extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Second() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/views/wasd.jsp");
+		request.setAttribute("label2", new String("label2 here"));
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ JA.TOMCAT/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>HOME</title>
+<!-- <style> -->
+<%--   	 <%@include file="/WEB-INF/viewe/css/main.css" %> --%>
+<!-- </style> -->
+</head>
+<body>
+<h3>HOME.JSP</h3>
+<%@ page import="java.util.*" %>
+<h2>Time on server: <%= new Date() %></h2>
+
+<h3><a href = "https://www.google.com.ua/?">Google</a></h3>
+<h3><a href = "D:\J\workspace\JA.TOMCAT\src\main\webapp\WEB-INF\views\home.jsp">H</a></h3>
+<h3><a href = "http://localhost:8080/JA.TOMCAT/welcome">W</a></h3>
+</body>
+</html>
